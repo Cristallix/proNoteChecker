a91e59f1f3892a0e85460bd520a7e4f020fd0f22
Jordan TRILLES <jordan.trilles@axa.fr>
2021-12-16 15:51:13 +0100

    feat : calcul de moyenne général

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Matiere, getMatieres } from "./services/note.services";
 
 import "./App.css";
 import "@axa-fr/react-toolkit-all/dist/style/af-toolkit-core.css";
+import { useEffect, useState } from "react";
 
 const recupererMatiere = async (callback: (matieres: Matiere[]) => void) => {
   const matieresApi = await getMatieres();
@@ -10,6 +11,21 @@ const recupererMatiere = async (callback: (matieres: Matiere[]) => void) => {
 };
 
 function App() {
+
+  const calculMoyenne = (matieres : Matiere[]) => {
+    const moyenneMatieres = matieres.map((matiere) => matiere.notes.reduce((a, b) => a + b, 0) / matiere.notes.length) ;
+
+    const moyenneGenerale = moyenneMatieres.reduce((a, b) => a + b, 0) / moyenneMatieres.length;
+
+    return Number(moyenneGenerale.toFixed(1));
+  }
+
+  const [matieres, setMatieres] = useState<Matiere[]>();
+
+  useEffect(() => {
+    recupererMatiere(setMatieres);    
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +34,7 @@ function App() {
       </header>
       <section>
         <FormNote onSucess={() => console.log('Super')} />
+        <h2>Moyenne générale : {matieres && calculMoyenne(matieres).toLocaleString("fr")}</h2>
       </section>
     </div>
   );
